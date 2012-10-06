be84c7d920f86449cb445577fcdef46f871e7306
Church <Matvey.Ezhov@gmail.com>
2012-10-07 01:00:18 +0400

    ami equity parser

---
+++ ami.py
@@ -0,0 +1,16 @@
+import csv
+import datetime
+from equity import EquityCurve
+
+def equity_ami(filename):
+    ''' Parse AmiBroker's trade list into EquityCurve '''
+    r = csv.reader(open(filename))
+    h = r.next()
+    di = h.index('Date')
+    ei = h.index('Cum. Profit')
+    eq = EquityCurve()
+    for l in r:
+        dt = datetime.datetime.strptime(l[di], '%d.%m.%Y %H:%M:%S')
+        eq.add_point(dt, float(l[ei]))
+    return eq
+        
