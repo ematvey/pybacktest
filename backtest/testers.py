f67261203cd07928635bbbd8f9287668d6dfeb8b
Church <Matvey.Ezhov@gmail.com>
2012-11-08 17:00:03 +0400

    Backtester made multi-asset-able (untested)

--- backtest/testers.py
+++ backtest/testers.py
@@ -2,7 +2,6 @@ import equity
 
 import copy
 import logging
-LOGGING_LEVEL = logging.INFO
 
 
 class Backtester(object):
@@ -10,10 +9,11 @@ class Backtester(object):
 
     def __init__(self, data, strategy_class,
                  strategy_args=tuple(), strategy_kwargs=dict(),
-                 run=True, log_level=None):
+                 run=True):
         '''
         Arguments
         ----
+
         * `strategy` should be Strategy class, i.e. has
             `process_datapoint` method that accepts datapoints and abstract
             `order_callback` method that accepts (contract, timestamp,
@@ -22,48 +22,92 @@ class Backtester(object):
         * `strategy_args`, `strategy_kwargs` - arguments for Strategy
             instantiation, specific for each strategy.
 
-        * `data` should be ab iterable over datapoints or over iterables over
-            datapoints (in case you're doing portfolio testing, e.g. on futures
-            chain.
+        * `data` - possible structures:
+
+          a) Sequence of datapoints, for simple 1-asset backtest:
+
+               [datapoint1, datapoint2, ..., datapointN]
+
+          b) Sequence of dicts, containing datapoints for multiple instruments,
+             for multi-asset backtest:
+
+               [{'instrument_1': datapoint1_i1,
+                 'instrument_2': datapoint1_i2}, ...]
+
+          You can also pass iterable over a) or b) to test on multiple
+          periods combined (e.g. on futures chain).
+
+          Note that datapoints are assumed to have `C` and `timestamp`
+          attributes (PositionalStrategy makes some additional assumptions).
+          You can change that by slightly adjusting `Backtester` class and used
+          Strategy classes.
+
+          If you need to pass some non-standard data to strategies, just attach
+          them as attributes to datapoints when preparing your data.
 
         Important attributes
         ----
+
         `full_curve` is an EquityCurve that tracks equity changes on every
             datapoint.
+
         `trades_curve` is an EquityCurve that tracks equity changes only on
             trades.
+
         '''
         self.strategy_class = strategy_class
         self.strategy_args = strategy_args
         self.strategy_kwargs = strategy_kwargs
         self.data = data if hasattr(data[0], '__iter__') else [data]
-        self.trades = []
+        self.multi = True if isinstance(data[0][0], dict) else False
         self.log = logging.getLogger(self.__class__.__name__)
-        self.log_level = log_level or LOGGING_LEVEL
-        self.log.setLevel(self.log_level)
-        calc = self._equity_calc = equity.EquityCalculator(log_level=log_level)
-        self.full_curve = calc.full_curve
-        self.trades_curve = calc.trades_curve
+        if not self.multi:
+            calc = self._equity = equity.EquityCalculator()
+            self._current_point = None
+            self.full_curve = calc.full_curve
+            self.trades_curve = calc.trades_curve
+        else:
+            self._equity = {}
+            self._current_point = {}
+            self.full_curve = equity.EquityCurve()
+            self.trades_curve = equity.EquityCurve()
         if run:
             self.run()
 
     def run(self):
         self.log.info('backtest started')
+        self.log.info('%s-asset mode', 'single' if self.multi else 'multi')
         for dataset in self.data:
-            s = self.strategy_class(#log_level=self.log_level,
-                                    *self.strategy_args,
+            s = self.strategy_class(*self.strategy_args,
                                     **self.strategy_kwargs)
             s.order_callback = self._matching_callback
             for datapoint in dataset:
-                self._current_point = datapoint
                 ### NOTE: datapoint is assumed to have `C` attr
                 ## change next string if your data is represented in other
                 ## way
-                self._equity_calc.new_price(datapoint.timestamp, datapoint.C)
+                if not self.multi:
+                    self._current_point = datapoint
+                    self._equity.new_price(datapoint.timestamp, datapoint.C)
+                else:
+                    for k, v in datapoint.iteritems():
+                        self._current_point[k] = v
+                        self._equity.setdefault(
+                            k, equity.EquityCalculator()
+                        ).new_price(v.timestamp, v.C)
                 s.process_data(datapoint)
             s.finalize()
-            self._equity_calc.merge()
-        self.log.info('backtest complete; results are in self.results')
+            if not self.multi:
+                self._equity.merge()
+            else:
+                for e in self._equity.values():
+                    e.merge()
+        for e in self._equity.values():
+            self.trades_curve.merge(e.trades_curve)
+            self.full_curve.merge(e.full_curve)
+        self.log.info('backtest complete')
+        self.log.info('see `trades_curve`, `full_curve` for aggregated curves')
+        if self.multi:
+            self.log.info('see results on individual instruments in `_equity`')
 
     def _matching_callback(self, order):
         ''' Match order from Strategy.
@@ -71,15 +115,21 @@ class Backtester(object):
             tuple. '''
         raise NotImplementedError
 
-    def _trade(self, timestamp, price, volume):
-        ''' Accept trades trades from `_matching_callback` or other
+    def _trade(self, timestamp, price, volume, instrument=None):
+        ''' Accept trades from `_matching_callback` or other
             matching engine. '''
-        cp = self._current_point
+        assert instrument or not self.multi, \
+            'Instrument required when multi-backtesting'
+        if self.multi:
+            assert instrument in self._current_point and \
+                instrument in self._equity, 'Wrong instrument'
+        cp = self._current_point if not self.multi else self._current_point[instrument]
+        eq = self._equity if not self.multi else self._equity[instrument]
         if price > cp.H or price < cp.L:
             self.log.error('requested trade price %s is not available '\
                            'in bar %s (H %s L %s)', price, cp.timestamp,
                            cp.H, cp.L)
-        self._equity_calc.new_trade(timestamp, price, volume)
+        eq.new_trade(timestamp, price, volume)
 
 
 class SimpleBacktester(Backtester):
