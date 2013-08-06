170873f9424d3acf9fe23e1ff4f8d48d8efd3856
Church <Matvey.Ezhov@gmail.com>
2013-08-07 00:00:02 +0400

    check_position_change fix

--- pybacktest/production.py
+++ pybacktest/production.py
@@ -15,7 +15,7 @@ def check_position_change(strategy_fn, ohlc):
     use `if position is None: execute`, not `if position: execute`, or you
     will never close your position. '''
 
-    pos = Backtest(strategy_fn, ohlc).positions    
+    pos = Backtest(strategy_fn(ohlc)).positions
     pos = pos.reindex(ohlc.index).ffill().fillna(value=0)
     
     if pos.iloc[-1] != pos.iloc[-2]:
