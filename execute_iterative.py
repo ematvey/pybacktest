c26e8f0387f814de28ad580f9806380b33e33914
Matvey Ezhov <matvey.ezhov@gmail.com>
2015-10-20 01:46:48 +0300

    execute_iterative

---
+++ execute_iterative.py
@@ -0,0 +1,11 @@
+def iterative_execute(data, signals):
+    # trade_price, positions, high=None, low=None, long_stoploss_price=None,
+    # long_takeprofit_price=None,
+    # short_stoploss_price=None, short_takeprofit_price=None
+
+    # high=data.get('high'), low=data.get('low'),
+    # long_stoploss_price=data.get('long_stoploss_price'),
+    # long_takeprofit_price=data.get('long_takeprofit_price'),
+    # short_stoploss_price=data.get('short_stoploss_price'),
+    # short_takeprofit_price=data.get('short_takeprofit_price')
+    raise NotImplementedError('strategy requires iterative execution which is not implented yet')
